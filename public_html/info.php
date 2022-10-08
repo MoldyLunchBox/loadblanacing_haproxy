71143db737a068a802e4270907cba246e27ca9a5
Abderrahmane Mya <amya@e1r10p1.1337.ma>
2022-10-08 08:48:54 +0100

    haproxy is configured for load balancing, and to protect against ddos attacks

---
+++ public_html/info.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>abdu says hello </h1>
+
+<?php
+echo "site 2";
+?>
+
+</body>
+</html>
---
+++ public_html/site1/info.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>abdu says hello </h1>
+
+<?php
+echo "site 1";
+?>
+
+</body>
+</html>
